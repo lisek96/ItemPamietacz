0464a1c24af82041786e7a2893b448575591c561
r.wojcik96@gmail.com <rafal.wojcik@ext.hlag.com>
2022-05-19 01:20:34 +0200

    another some work

--- app/src/main/java/rafalwojcik/prm/MainFragment.kt
+++ app/src/main/java/rafalwojcik/prm/MainFragment.kt
@@ -2,6 +2,7 @@ package rafalwojcik.prm
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
@@ -17,11 +18,26 @@ class MainFragment : Fragment() {
     ): View {
         return MainFragmentBinding.inflate(
             inflater, container, false
-        ).also { binding = it }.root
+        ).apply {
+            binding = this
+            binding.bottomNavigationView.setOnItemSelectedListener { actOnOptionSelected(it) }
+        }.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+    }
+
+    fun actOnOptionSelected(item : MenuItem) : Boolean{
+        when(item.itemId){
+            R.id.load_photo -> print("xx")
+            R.id.take_photo -> openPhotoCaptureFragment()
+        }
+        return true
+    }
+
+    fun openPhotoCaptureFragment(){
+        (activity as MainActivity).goTakePhoto()
     }
 }
